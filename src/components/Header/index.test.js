a30d89fcb703b2f61c25cc3ae22d09bf346e3a30
stefano <stefanosemail@protonmail.com>
2021-03-02 22:50:03 +0000

    [lint]&jtest ok

--- src/components/Header/index.test.js
+++ src/components/Header/index.test.js
@@ -4,7 +4,7 @@ import Header from './'
 import { findDataTestAttr } from '../../test/testUtils'
 
 const defaultProps = {
-  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }],
+  guessedWords: [{ guessedWord: 'train', letterMatchCount: 3 }]
 }
 
 /**
@@ -42,7 +42,7 @@ describe('if there are words guessed', () => {
   const guessedWords = [
     { guessWord: 'jockey', letterMatchCount: 2, success: false },
     { guessWord: 'saint', letterMatchCount: 1, success: false },
-    { guessWord: 'party', letterMatchCount: 5, success: false },
+    { guessWord: 'party', letterMatchCount: 5, success: false }
   ]
   beforeEach(() => {
     wrapper = setup({ guessedWords })
@@ -57,7 +57,7 @@ describe('if there are words guessed', () => {
     wrapper = setup({
       guessWord: 'party',
       letterMatchCount: 5,
-      success: true,
+      success: true
     })
     const success = findDataTestAttr(wrapper, 'guessed-word-correctly')
     expect(success.text()).toBe('Congratulations! You guessed the word!')
